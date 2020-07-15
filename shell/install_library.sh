818a6c682d7e13f27b7c443873bf5d28e22666c1
anyuhanfei <1223050251@qq.com>
2020-07-15 15:18:16 +0800

    完善软件和第三方库自动安装的shell文件

--- shell/install_library.sh
+++ shell/install_library.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+# 安装python,screen
+# 判断服务器类型, 当前可判断类型: Ubuntu/Debian, RHEL/CentOS/Fedora, OpenSUSE, ArchLinux
+is_debian = `apt-get | wc -l | head -n 2 | tail -n 1`
+if $is_debian > 1
+then
+	apt-get install python3
+	apt-get install screen
+fi
+is_contos = `yum | wc -l | head -n 2 | tail -n 1`
+if $is_contos > 1
+then
+	yum install python3
+	yum install screen
+fi
+is_opensuse = `zypper | wc -l | head -n 2 | tail -n 1`
+if $is_opensuse > 1
+then
+	zypper install python3
+	zypper install screen
+fi
+is_archLinux = `pacman | wc -l | head -n 2 | tail -n 1`
+if $is_archLinux > 1
+then
+	pacman install python3
+	pacman install screen
+fi
+
 #安装python第三方库
 libs=(requests pymysql DBUtils redis)
 
