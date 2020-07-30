b5a84fe44e730b42cf96efde77665902732c1859
anyuhanfei <1223050251@qq.com>
2020-07-30 10:28:43 +0800

    规范书写

--- kline_index.py
+++ kline_index.py
@@ -6,14 +6,14 @@ import time
 
 from config import config
 from utils import k
-from config.mysql连接 import MYSQL连接 as mysql_link
+from config import mysql连接
 
 
 if __name__ == "__main__":
     jobs = []
     for coin_type in config.K线图币种对:
         number = (len(jobs) % len(config.K线图币种对)) + 1
-        child_process = multiprocessing.Process(target=k.timekeeping, args=(coin_type, number, mysql_link(), ))
+        child_process = multiprocessing.Process(target=k.timekeeping, args=(coin_type, number, mysql连接.MYSQL连接(), ))
         child_process.daemon = True
         jobs.append(child_process)
         child_process.start()
