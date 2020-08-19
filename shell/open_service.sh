4621f43ff4e64f39944507e7106a3e0764fbbaeb
暗语寒飞 <1223050251@qq.com>
2020-08-19 13:58:16 +0800

    Update open_service.sh

--- shell/open_service.sh
+++ shell/open_service.sh
@@ -6,9 +6,9 @@ screen -S data -X quit
 sleep 1s
 
 screen -Sdm k
-screen -x -S k -p 0 -X stuff "cd /root/huobi_global_data_acquisition-master/\n"
+screen -x -S k -p 0 -X stuff "cd /root/tools/huobi_global_data_acquisition-master/\n"
 screen -x -S k -p 0 -X stuff "python3 kline_index.py\n"
 
 screen -Sdm data
-screen -x -S data -p 0 -X stuff "cd /root/huobi_global_data_acquisition-master/\n"
+screen -x -S data -p 0 -X stuff "cd /root/tools/huobi_global_data_acquisition-master/\n"
 screen -x -S data -p 0 -X stuff "python3 data_index.py\n"
