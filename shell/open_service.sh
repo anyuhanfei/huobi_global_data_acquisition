6f3c0e56d197458e54461717c445415b0490d29e
漠北-暗语寒飞 <1223050251@qq.com>
2020-12-30 10:56:22 +0800

    启动脚本自动识别路径

--- shell/open_service.sh
+++ shell/open_service.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+dir=$(cd "$(dirname "$0")"; pwd)
+dir=${dir%/*}
 
 screen -S k -X quit
 screen -S data -X quit
@@ -6,9 +8,9 @@ screen -S data -X quit
 sleep 1s
 
 screen -Sdm k
-screen -x -S k -p 0 -X stuff "cd /root/tools/huobi_global_data_acquisition-master/\n"
+screen -x -S k -p 0 -X stuff "cd ${dir}\n"
 screen -x -S k -p 0 -X stuff "python3 kline_index.py\n"
 
 screen -Sdm data
-screen -x -S data -p 0 -X stuff "cd /root/tools/huobi_global_data_acquisition-master/\n"
+screen -x -S data -p 0 -X stuff "cd ${dir}\n"
 screen -x -S data -p 0 -X stuff "python3 data_index.py\n"
